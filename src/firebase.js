e4677e83267e3855cafc3d7a239de541263dd41f
Karamveer200 <68271221+Karamveer200@users.noreply.github.com>
2021-07-20 00:14:52 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import "firebase/firestore";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzTkRhoccp3Jz1f_ycILExKvVYKXM55P4",
-  authDomain: "linkedin-clone-b1c62.firebaseapp.com",
-  projectId: "linkedin-clone-b1c62",
-  storageBucket: "linkedin-clone-b1c62.appspot.com",
-  messagingSenderId: "771095666354",
-  appId: "1:771095666354:web:35712350f47c97bffab3ab",
-  measurementId: "G-557D8T6QFL",
+  
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
